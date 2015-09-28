104ed2dcf9c8d593febb3848067b3f23b648a19c
Kevin Gautreau <contact@kgaut.net>
2015-09-28 17:01:48 +0200

    typo

--- menu/create-a-menu.php
+++ menu/create-a-menu.php
@@ -4,7 +4,7 @@ $menu = array (
   'menu_name' => 'menu_test_one',
   'title' => 'My Menu One',
   'description' => 'Lorem Ipsum',
-),
+);
 
 $exists = db_query("SELECT title FROM {menu_custom} WHERE menu_name=:menu_name", array(':menu_name' => $menu['menu_name']))->fetchField();
 if (!$exists) {
